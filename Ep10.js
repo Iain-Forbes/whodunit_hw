a44f705dc0e935525c7f0e917729969272df41c1
Iain Forbes <i.forbes77@gmail.com>
2020-12-07 18:32:56 +0000

    few changes ep10

--- Ep10.js
+++ Ep10.js
@@ -3,24 +3,24 @@ const scenario = {
     room: 'Conservatory',
     weapon: 'Lead Pipe'
   };
-
-    const plotTwist = function(){
+    
+scenario.murderer = "Miss Scarlett"
+    
+const plotTwist = function(){
     
     if (scenario.murderer === 'Mrs. White');
     {
         scenario.murderer = 'Colonel Mustard';
     }
+
     }
-    
 
-    scenario.murderer = "Miss Scarlett"
-
-    const stichUp = function(){
+const stichUp = function(){
     if (scenario.murderer === 'Colonel Mustard');
     {
         scenario.murderer = 'Professor Plum';
     }
-
+    
     }
     
 
@@ -30,7 +30,6 @@ const declareMurderer = function() {
 
 
 plotTwist(); 
-scenario.murderer = "Miss Scarlett"
 stichUp();
 const verdict = declareMurderer();
 console.log(verdict);
